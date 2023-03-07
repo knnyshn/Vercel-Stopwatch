e73bc41eef9e26351bb4cb876572f797afe1ce6d
Kenny <knnyshn@gmail.com>
2023-03-07 13:29:48 -0500

    React Stopwatch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Stopwatch from "./Stopwatch";
+import './App.css';
+
+function App() {
+  return (
+    <Stopwatch />
+  );
+}
+
+export default App;
